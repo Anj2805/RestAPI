7aeb7134f154d5a5657e079a47f115cc20dc9c65
Anj2805 <anjalis823001@gmail.com>
2025-07-04 16:42:00 +0530

    Initial commit

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const url = 'mongodb+srv://User1:nV67aqCLmiNlYNW2@cluster0.hqtzbir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const url = ''
 
 const connection = async () =>{
     return mongoose.connect(url)
